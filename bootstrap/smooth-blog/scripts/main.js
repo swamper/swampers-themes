6b60b13eb1dc93cd77b710a189aaee8465598048
swamper <swamper838@gmail.com>
2015-03-17 11:56:26 -0500

    Fixed scripts

--- bootstrap/smooth-blog/scripts/main.js
+++ bootstrap/smooth-blog/scripts/main.js
@@ -1,11 +1,17 @@
 /* jshint devel:true */
-'use strict';
+
+
 // Scroll to top button
 // Only enable if the document has a long scroll bar
 // Note the window height + offset
-if ( ($(window).height() + 100) < $(document).height() ) {
-    $('#scroll-to-top').removeClass('hidden').affix({
-        // how far to scroll down before link "slides" into view
-        offset: {top:100}
-    });
-}
+;(function(window, document) {
+    'use strict';
+    if (($(window).height() + 100) < $(document).height()) {
+        $('#scroll-to-top').removeClass('hidden').affix({
+            // how far to scroll down before link "slides" into view
+            offset: {
+                top: 100
+            }
+        });
+    }
+})(window, document);
